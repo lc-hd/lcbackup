7f55c50b0be40dbe069fabf1b92cd2f2016c5118
Hadron DaVinci <hd>
2023-12-18 00:15:44 -0500

    feat: updated example env file

--- app/env-local.example.sh
+++ app/env-local.example.sh
@@ -1,7 +1,20 @@
 # This is an example of the environment variables of interest
 # Dockerfile will source env-local.sh
+
+# ----for dev of prod environments
+DEBUG_VALUE=
+
+# --------------for connecting to db
 export DB_NAME=
 export DB_USER=
 export DB_PASS=
 export DB_HOST=
 export DB_PORT=
+# --------------for connecting to db
+
+# -------------------------------------for connecting to s3 bucket
+export DBBACKUP_ACCESS_KEY=
+export DBBACKUP_BUCKET_NAME=
+export DBBACKUP_ENDPOINT_URL=
+export DBBACKUP_SECRET_KEY=
+# -------------------------------------for connecting to s3 bucket
